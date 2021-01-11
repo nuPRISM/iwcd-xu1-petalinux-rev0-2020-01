330150d7b34c17977bccefdab107d8dbdaf64e3c
Andrzej Buchowicz <A.Buchowicz@ire.pw.edu.pl>
2021-01-11 11:15:08 +0100

    all ones/zeros test patterns added do adc.c

--- wut_petalinux/project-spec/meta-user/recipes-apps/adc/files/adc.c
+++ wut_petalinux/project-spec/meta-user/recipes-apps/adc/files/adc.c
@@ -61,6 +61,11 @@
 #define RESET_GPIO 497
 #define PDN_GPIO   498
 
+#define NORMAL_OP_TEST_PATTERN 0x00
+#define ALL_ZEROS_TEST_PATTERN 0x11
+#define ALL_ONES_TEST_PATTERN  0x22
+#define TOGGLE_TEST_PATTERN    0x33
+
 static uint32_t mode;
 static uint8_t bits = 8;
 static uint32_t speed = 500000;
@@ -203,7 +208,7 @@ int adc_nominal_mode(int fd, int adc_num) {
 
     adc_enable(adc_num, true);
 
-    int ret = adc_write(fd, 0x06, 0x00);    // Register 06h: normal output
+    int ret = adc_write(fd, 0x06, 0x00);          // Register 06h: normal output
     DBG("adc_test: reg 06h, ret=%d\n", ret);
     
     adc_enable(adc_num, false);
@@ -212,16 +217,16 @@ int adc_nominal_mode(int fd, int adc_num) {
 }
 
 
-int adc_test(int fd, int adc_num) {
+int adc_test(int fd, int adc_num, uint8_t test_pattern) {
     DBG("adc_test(): num=%d\n", adc_num);
 
     adc_enable(adc_num, true);
 
-    int ret = adc_write(fd, 0x06, 0x02);    // Register 06h: Test pattern output enabled
+    int ret = adc_write(fd, 0x06, 0x02);           // Register 06h: Test pattern output enabled
     DBG("adc_test: reg 06h, ret=%d\n", ret);
-    ret = adc_write(fd, 0x0A, 0x33);       // Register 0Ah: test pattern for channel A, B Toggle pattern: data alternate between 101010101010 and 010101010101 
+    ret = adc_write(fd, 0x0A, test_pattern);       // Register 0Ah: test pattern for channel A, B Toggle pattern: data alternate between 101010101010 and 010101010101 
     DBG("adc_test: reg 0Ah, ret=%d\n", ret);
-    ret = adc_write(fd, 0x0B, 0x33);       // Register 0Ah: test pattern for channel C, D Toggle pattern: data alternate between 101010101010 and 010101010101 
+    ret = adc_write(fd, 0x0B, test_pattern);       // Register 0Ah: test pattern for channel C, D Toggle pattern: data alternate between 101010101010 and 010101010101 
     DBG("adc_test: reg 0AB, ret=%d\n", ret);
 
     adc_enable(adc_num, false);
@@ -263,7 +268,9 @@ void print_usage() {
     printf("\tpup  - power up\n");
     printf("\trst  - reset\n");
     printf("\tnom  - nominal mode(?)\n");
-    printf("\ttst  - test\n");
+    printf("\ttst  - toggle test pattern\n");
+    printf("\ttst0 - all zeros test pattern\n");
+    printf("\ttst1 - all ones test pattern\n");
     printf("\tinit - initialization\n");
     printf("\tss   - set SPI slave mode(?)\n");
     printf("\tnss  - unset SPI slave mode(?)\n");
@@ -364,7 +371,11 @@ int main(int argc, char **argv)
         } else if(strcmp(argv[optind], "nom") == 0) {
             ret = adc_nominal_mode(fd, adc_num);
         } else if(strcmp(argv[optind], "tst") == 0) {
-            ret = adc_test(fd, adc_num);
+            ret = adc_test(fd, adc_num, TOGGLE_TEST_PATTERN);
+        } else if(strcmp(argv[optind], "tst0") == 0) {
+            ret = adc_test(fd, adc_num, ALL_ZEROS_TEST_PATTERN);
+        } else if(strcmp(argv[optind], "tst1") == 0) {
+            ret = adc_test(fd, adc_num, ALL_ONES_TEST_PATTERN);
         } else if(strcmp(argv[optind], "init") == 0) {
             ret = adc_init(fd, adc_num);
         } else if(strcmp(argv[optind], "ss") == 0) {
