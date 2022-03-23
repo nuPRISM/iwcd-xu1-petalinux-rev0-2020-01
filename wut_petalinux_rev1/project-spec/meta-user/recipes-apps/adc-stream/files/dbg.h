47ac086b473880fcfd19ab56904bd966b70cda7c
Andrzej Buchowicz <A.Buchowicz@ire.pw.edu.pl>
2022-03-23 10:43:39 +0100

    old/unused apps removed from repo

--- wut_petalinux_rev1/project-spec/meta-user/recipes-apps/adc-stream/files/dbg.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEBUG_H
-#define DEBUG_H
-    #ifdef DEBUG
-        #define DBG(fmt, args...) printf("DBG: " fmt, args)                                                                          
-    #else
-        #define DBG(fmt, args...)
-    #endif
-#endif
